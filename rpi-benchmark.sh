d7446fdb5d8ae45e7deae5153c146e174dff30f7
aikoncwd <aikoncwd@users.noreply.github.com>
2016-05-27 20:54:22 +0200

    Update rpi-benchmark.sh

--- rpi-benchmark.sh
+++ rpi-benchmark.sh
@@ -13,7 +13,7 @@ fi
 # Script start!
 clear
 sync
-printf "Raspberry Pi microSD Benchmark\n"
+printf "Raspberry Pi Benchmark Test\n"
 printf "Author: AikonCWD\n\n"
 
 # Show current hardware
@@ -23,17 +23,17 @@ vcgencmd get_config int | grep core_freq
 vcgencmd get_config int | grep sdram_freq
 printf "microsd_clock="
 grep "actual clock" /sys/kernel/debug/mmc0/ios 2>/dev/null | awk '{printf("%0.3f MHz", $3/1000000)}'
-printf "\n"
+printf "\n\n"
 
-printf "Running CPU test..."
+printf "Running CPU test...\n"
 sysbench --num-threads=4 --validate=on --test=cpu --cpu-max-prime=5000 run | grep "total time:"
 printf "\n"
 
-printf "Running MEMORY test..."
+printf "Running MEMORY test...\n"
 sysbench --num-threads=4 --validate=on --test=memory --memory-block-size=1K --memory-total-size=3G --memory-access-mode=seq run | grep "total time:"
 printf "\n"
 
-printf "Running THREADS test..."
+printf "Running THREADS test...\n"
 sysbench --num-threads=4 --validate=on --test=threads --thread-yields=3000 run | grep "total time:"
 printf "\n"
 
@@ -48,6 +48,7 @@ printf "\n"
 printf "Running SD READ test...\n"
 echo 3 > /proc/sys/vm/drop_caches && sync && dd if=~/test.tmp of=/dev/null bs=1M 2>&1 | grep -v records
 rm -f ~/test.tmp
-printf "\n"
+printf "\n\n"
 
-printf "AikonCWD's Benchmark completed!\n\n"
+vcgencmd measure_temp
+printf "AikonCWD's rpi-benchmark completed!\n\n"
