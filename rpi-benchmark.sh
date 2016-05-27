5d298287f748d84c8eddcf855d0e53796cbd3ba3
aikoncwd <aikoncwd@users.noreply.github.com>
2016-05-27 20:24:40 +0200

    Update rpi-benchmark.sh

--- rpi-benchmark.sh
+++ rpi-benchmark.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-printf "\n"
-printf "Raspberry Pi microSD Benshmakrs\n"
-
-CLOCK="$(grep "actual clock" /sys/kernel/debug/mmc0/ios 2>/dev/null | awk '{printf("%0.3f MHz", $3/1000000)}')"
-if [ -n "$CLOCK" ]; then
-  echo "microSD clock: $CLOCK"
-fi
-printf "\n"
-
-# Install dependencies.
+# Install dependencies
 if [ ! `which hdparm` ]; then
-  printf "Installing hdparm...\n"
   apt-get install -y hdparm
-  printf "Install complete!\n\n"
 fi
+
+# Script start!
+clear
+printf "\n"
+printf "Raspberry Pi microSD Benchmark\n"
+printf "Author: AikonCWD\n"
+
+# Show current SD clock
+CLOCK="$(grep "actual clock" /sys/kernel/debug/mmc0/ios 2>/dev/null | awk '{printf("%0.3f MHz", $3/1000000)}')"
+printf "microSD clock: $CLOCK\n"
 
 # Run benchmarks.
-printf "Running hdparm test...\n"
-hdparm -t /dev/mmcblk0
+printf "Running HDPARM test...\n"
+hdparm -t --direct /dev/mmcblk0
 printf "\n"
 
-printf "Running dd test...\n\n"
-dd if=/dev/zero of=~/test.tmp bs=8k count=50k conv=fsync; rm -f ~/test.tmp
+printf "Running DD WRITE test...\n\n"
+dd if=/dev/zero of=~/test.tmp bs=1M count=512M conv=fsync
 printf "\n"
+
+printf "Running DD READ test...\n\n"
+dd if=~/test.tmp of=/dev/null bs=1M; rm -f ~/test.tmp
+printf "\n"
+
 
 printf "microSD card benchmark complete!\n\n"
