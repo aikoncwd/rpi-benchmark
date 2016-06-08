7dfd67f7bfa59d02244db63c9ab59ab799efc36a
aikoncwd <aikoncwd@users.noreply.github.com>
2016-06-08 19:43:20 +0200

    Update rpi-benchmark.sh

--- rpi-benchmark.sh
+++ rpi-benchmark.sh
@@ -26,53 +26,43 @@ vcgencmd get_config int | grep arm_freq
 vcgencmd get_config int | grep core_freq
 vcgencmd get_config int | grep sdram_freq
 vcgencmd get_config int | grep gpu_freq
-echo -e "microsd_clock="
+printf "microsd_clock="
 grep "actual clock" /sys/kernel/debug/mmc0/ios 2>/dev/null | awk '{printf("%0.3f MHz", $3/1000000)}'
-echo -e "\n\n"
+echo -e "\n"
 
-echo -e "Running InternetSpeed test...\n"
-echo -e "\e[93m"
+echo -e "Running InternetSpeed test...\e[93m"
 speedtest-cli --simple
 echo -e "\e[0m"
 
-echo -e "Running CPU test...\n"
+echo -e "Running CPU test...\e[93m"
 sysbench --num-threads=4 --validate=on --test=cpu --cpu-max-prime=500 run | grep "total time:"
-echo -e "    "
 vcgencmd measure_temp
-echo -e "\n"
+echo -e "\e[0m"
 
-echo -e "Running MEMORY test...\n"
+echo -e "Running MEMORY test...\e[93m"
 sysbench --num-threads=4 --validate=on --test=memory --memory-block-size=1K --memory-total-size=1G --memory-access-mode=seq run | grep "total time:"
-echo -e "    "
 vcgencmd measure_temp
-echo -e "\n"
+echo -e "\e[0m"
 
-echo -e "Running THREADS test...\n"
+echo -e "Running THREADS test...\e[93m"
 sysbench --num-threads=4 --validate=on --test=threads --thread-yields=300 run | grep "total time:"
-echo -e "    "
 vcgencmd measure_temp
-echo -e "\n"
+echo -e "\e[0m"
 
-echo -e "Running HDPARM test...\n"
-echo -e "   "
+echo -e "Running HDPARM test...\e[93m"
 hdparm -t /dev/mmcblk0 | grep Timing
-echo -e "    "
 vcgencmd measure_temp
-echo -e "\n"
+echo -e "\e[0m"
 
-echo -e "Running DD WRITE test...\n"
-echo -e "    "
+echo -e "Running DD WRITE test...\e[93m"
 rm -f ~/test.tmp && sync && dd if=/dev/zero of=~/test.tmp bs=1M count=5 conv=fsync 2>&1 | grep -v records
-echo -e "    "
 vcgencmd measure_temp
-echo -e "\n"
+echo -e "\e[0m"
 
-echo -e "Running DD READ test...\n"
-echo -e "    "
+echo -e "Running DD READ test...\e[93m"
 echo -e 3 > /proc/sys/vm/drop_caches && sync && dd if=~/test.tmp of=/dev/null bs=1M 2>&1 | grep -v records
-echo -e "    "
 vcgencmd measure_temp
 rm -f ~/test.tmp
-echo -e "\n\n"
+echo -e "\e[0m\n"
 
 echo -e "AikonCWD's rpi-benchmark completed!\n"
