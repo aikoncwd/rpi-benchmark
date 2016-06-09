065dd26274b8fb6be7031822ab210900c15fd818
aikoncwd <aikoncwd@users.noreply.github.com>
2016-06-09 20:07:05 +0200

    Update rpi-benchmark.sh

--- rpi-benchmark.sh
+++ rpi-benchmark.sh
@@ -2,17 +2,6 @@
 
 [ "$(whoami)" == "root" ] || { echo "Must be run as sudo!"; exit 1; }
 
-# Internal functions
-spinner() {
-    local i sp n
-    sp="/-\|"
-    n=${#sp}
-    printf ""
-    while sleep 0.1; do
-        printf "%s\b" "${sp:i++%n:1}"
-    done
-}
-
 # Install dependencies
 if [ ! `which hdparm` ]; then
   apt-get install -y hdparm
@@ -41,40 +30,40 @@ printf "sd_clock="
 grep "actual clock" /sys/kernel/debug/mmc0/ios 2>/dev/null | awk '{printf("%0.3f MHz", $3/1000000)}'
 echo -e "\n\e[0m"
 
-echo -e "Running InternetSpeed test...\e[93m" & spinner
+echo -e "Running InternetSpeed test...\e[93m"
 speedtest-cli --simple
 echo -e "\e[0m"
 
 echo -e "Running CPU test...\e[93m"
-sysbench --num-threads=4 --validate=on --test=cpu --cpu-max-prime=5000 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:] & spinner
+sysbench --num-threads=4 --validate=on --test=cpu --cpu-max-prime=5000 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
 echo -e ""
 vcgencmd measure_temp
 echo -e "\e[0m"
 
 echo -e "Running THREADS test...\e[93m"
-sysbench --num-threads=4 --validate=on --test=threads --thread-yields=4000 --thread-locks=6 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:] & spinner
+sysbench --num-threads=4 --validate=on --test=threads --thread-yields=4000 --thread-locks=6 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
 echo -e ""
 vcgencmd measure_temp
 echo -e "\e[0m"
 
 echo -e "Running MEMORY test...\e[93m"
-sysbench --num-threads=4 --validate=on --test=memory --memory-block-size=1K --memory-total-size=3G --memory-access-mode=seq run | grep 'Operations\|transferred\|total time:\|min:\|avg:\|max:' | tr -s [:space:] & spinner
+sysbench --num-threads=4 --validate=on --test=memory --memory-block-size=1K --memory-total-size=3G --memory-access-mode=seq run | grep 'Operations\|transferred\|total time:\|min:\|avg:\|max:' | tr -s [:space:]
 echo -e ""
 vcgencmd measure_temp
 echo -e "\e[0m"
 
 echo -e "Running HDPARM test...\e[93m"
-hdparm -t /dev/mmcblk0 | grep Timing & spinner
+hdparm -t /dev/mmcblk0 | grep Timing
 vcgencmd measure_temp
 echo -e "\e[0m"
 
 echo -e "Running DD WRITE test...\e[93m"
-rm -f ~/test.tmp && sync && dd if=/dev/zero of=~/test.tmp bs=1M count=512 conv=fsync 2>&1 | grep -v records & spinner
+rm -f ~/test.tmp && sync && dd if=/dev/zero of=~/test.tmp bs=1M count=512 conv=fsync 2>&1 | grep -v records
 vcgencmd measure_temp
 echo -e "\e[0m"
 
 echo -e "Running DD READ test...\e[93m"
-echo -e 3 > /proc/sys/vm/drop_caches && sync && dd if=~/test.tmp of=/dev/null bs=1M 2>&1 | grep -v records & spinner
+echo -e 3 > /proc/sys/vm/drop_caches && sync && dd if=~/test.tmp of=/dev/null bs=1M 2>&1 | grep -v records
 vcgencmd measure_temp
 rm -f ~/test.tmp
 echo -e "\e[0m"
